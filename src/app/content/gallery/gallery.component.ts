78773d0986c3f5d2eb112385da142324bdf93484
Yana <zzizzu@ukr.net>
2019-09-23 11:04:40 +0300

    create slider

--- src/app/content/gallery/gallery.component.ts
+++ src/app/content/gallery/gallery.component.ts
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GalleryComponent implements OnInit {
 
+  gallery: string[] = [
+    'https://media.wired.com/photos/5c1ae77ae91b067f6d57dec0/master/w_2560%2Cc_limit/Comparison-City-MAIN-ART.jpg',
+    'https://media.wired.com/photos/5c1ae525da84021ed9889554/master/w_1600%2Cc_limit/Compare-1-nightsight_iPhoneXSMax-Google.jpg',
+    'https://eoimages.gsfc.nasa.gov/images/imagerecords/145000/145619/spaindolmen_oli_2013205.jpg'
+  ];
+  i: number = 0;
+  currentImg: string;
   constructor() { }
 
   ngOnInit() {
+    this.currentImg = this.gallery[0];
   }
 
+  next(){
+    this.i++;
+    if (this.i >= this.gallery.length) {
+      this.i = 0;
+    }
+    this.currentImg = this.gallery[this.i];
+    console.log('next' + this.i)
+  }
+  prev(){
+    this.i--;
+    if (this.i < 0) {
+      this.i = this.gallery.length - 1;
+    }
+    this.currentImg = this.gallery[this.i];
+    console.log('prev' + this.i)
+  }
 }
